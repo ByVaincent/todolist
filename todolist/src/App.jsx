7acb10d97c26b8665c4d954a06fff1eaa10a3ee5
Vincent Jacquelin <vjacquelinpro@gmail.com>
2024-12-03 09:23:13 +0100

    Submit ok, travaille sur les sélections de tâches

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,21 +1,24 @@
-import { useId, useState } from 'react'
+import { useId, useRef, useState } from 'react'
 import { Input } from './form/Input'
 
-const taskInit = [
+const tasksInit = [
   {
       "id": 1,
       "name": "Faire les courses",
-      "completed": false
+      "completed": false,
+      "selected": false
   },
   {
       "id": 2,
       "name": "Mettre de l'essence",
-      "completed": true
+      "completed": true,
+      "selected": false
   },
   {
       "id": 3,
       "name": "Appeler grand mère",
-      "completed": false
+      "completed": false,
+      "selected": false
   }
 ]
 
@@ -23,21 +26,48 @@ const taskInit = [
 
 function App() {
 
-  const [tasks, setTasks] = useState(taskInit)
+  const [tasks, setTasks] = useState(tasksInit)
+
+  const addNewTask = (e) => {
+    e.preventDefault()
+
+    const newTask = {
+      "id": Date.now(),
+      "name": e.target.new_task.value,
+      "completed": false,
+      "selected": false
+    }
+
+    if(e.target.new_task.value){
+      setTasks([...tasks, newTask])
+    } else{
+      alert("Merci dindiquer correctement une tâche")
+    }
+
+    e.target.new_task.value = ''
+  }
+
+
+  const selectTasks = (e) => {
+    // const tasksSelected = tasks.map((task) => {
+    //   if (task.id === e.target.id)
+    // })
+   console.log(e.target.checked)
+  }
+
 
 
   return (
     <>
-      <form onSubmit={setTasks(updateTasks())}>
-        <Input
-
+      <form onSubmit={addNewTask}>
+        <input
           placeholder="Nouvelle tâche"
           name="new_task"
           id="new_task"
         />
         <button type='submit'>Créer la nouvelle tâche</button>
       </form>
-      <DisplayTasks tasks={tasks}/>
+      <DisplayTasks tasks={tasks} onChange={selectTasks}/>
       
 
     </>
@@ -45,26 +75,24 @@ function App() {
 }
 
 
-function DisplayTasks({tasks}){
+function DisplayTasks({tasks, onChange}){
   return <div>
     <h1>Liste des tâches </h1>
     {tasks.map((task) => {
       return <div key={task.id}>
-        {task.name}: {task.completed ? "ok":"A faire"}
+        <input 
+          type="checkbox" 
+          id={task.id}
+          onChange={onChange}
+
+          />
+        {task.name}: {task.completed ? " OK":" À faire"}
       </div>
     })}
   </div>
 }
 
-function updateTasks(event, tasks){
-  event.preventDefault()
-  const task = {
-    "id": Date.now(),
-    "name": event.target.new_task.value,
-    "completed": false
-  }
-  tasks.push(task)
-  
-}
+
 
 export default App
+

83e8dce7b5cbd068d8df72c9af68a8caed1aafe4
Vincent Jacquelin <vjacquelinpro@gmail.com>
2025-01-17 16:31:05 +0100

    ajout du fichier .env

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import Task from './Models/Task.js'
 import mongoose from 'mongoose';
+import router from './routes/tasks.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.8')
+mongoose.connect(process.env.API_CONNECTION_STRING)
     .then(() => console.log('Connexion à mongoDB ok'))
     .catch(() => console.log('Connexion à mongoDB failed'))
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/tasks', (req, res, next) => {
-    Task.find()
-    .then(tasks => res.status(200).json(tasks))
-    .catch(error => res.status(400).json({error}))
-});
+const tasksRoutes = router;
+app.use('/api/tasks', tasksRoutes);
 
-app.post('/api/tasks', (req, res, next) => {
-    const newTask = new Task({
-        ...req.body
-    });
-    newTask.save()
-        .then((task) => res.status(200).json({message: `Nouvelle tâche créée avec succès: ${task}`}))
-        .catch(error => res.status(400).json({error}));
-});
 
-app.put('/api/tasks/:id/:completed', (req, res, next) => {
-    Task.updateOne({id: req.params.id}, {completed: req.params.completed})
-    .then(() => res.status(200).json({message: "Task updated!"}))
-})
-
-app.delete('/api/tasks/div_:id', (req, res, next) => {
-    Task.deleteOne({id: req.params.id})
-        .then(() => res.status(200).json({message: 'Tâche supprimée avec succès!'}))
-        .catch(error => res.status(400).json({error}))
-})
 
 
 

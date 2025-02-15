591ad8faea5fac39395569af904bbe7ac07d39d0
Vincent Jacquelin <vjacquelinpro@gmail.com>
2025-02-15 11:07:23 +0100

    Changement des adresses pour le déploiement

--- frontend/src/functions/authFunctions.jsx
+++ frontend/src/functions/authFunctions.jsx
@@ -1,4 +1,4 @@
-const SERVER_URL = "http://localhost:4000"; //"https://todolist-server-h39x.onrender.com"
+const SERVER_URL = "https://todolist-server-h39x.onrender.com"; //"http://localhost:4000";
 
 export async function logInFunction(v, setAuthentication, setUserId) {
   const body = JSON.stringify({
